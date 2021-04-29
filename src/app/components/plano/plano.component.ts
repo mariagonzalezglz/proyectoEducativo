3f720d2404df56aa803f853bfb07945da789d396
Alejandro Tellez <telles977@gmail.com>
2021-04-28 21:00:06 -0500

    “Ima”

--- src/app/components/plano/plano.component.ts
+++ src/app/components/plano/plano.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-plano',
@@ -12,4 +13,48 @@ export class PlanoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  name = "Angular";
+  @ViewChild("videoPlayer", { static: false }) videoplayer: ElementRef;
+  isPlay: boolean = false;
+  
+  
+  toggleVideo() {
+    this.videoplayer.nativeElement.play();
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 320;
+  }
+  playPause() {
+    var myVideo: any = document.getElementById("my_video_1");
+    if (myVideo.paused) myVideo.play();
+    else myVideo.pause();
+  }
+
+  makeBig() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 560;
+  }
+
+  makeSmall() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 320;
+  }
+
+  makeNormal() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 420;
+  }
+
+  skip(value) {
+    let video: any = document.getElementById("my_video_1");
+    video.currentTime += value;
+  }
+
+  restart() {
+    let video: any = document.getElementById("my_video_1");
+    video.currentTime = 0;
+  }
 }
+
+
+
